592ccc7afa23742862d59c2535e7aa3abd8a21ea
paulusaditya <133630839+paulusaditya@users.noreply.github.com>
2024-01-15 23:45:55 +0700

    persegi panjang

--- js/script.js
+++ js/script.js
@@ -12,8 +12,43 @@ function hitungKelilingPersegi() {
     document.getElementById('keliling').innerText = outputText;
 }
 
-function resetInput() {
+function resetInput1() {
     document.getElementById('sisi').value = '';
     document.getElementById('luas').innerText = '';
     document.getElementById('keliling').innerText = '';
+
+}
+
+function halamanini() {
+    window.location.href = 'index.html';
+}
+
+function halamanpersegipanjang() {
+    window.location.href = 'persegipanjang.html';
+}
+
+function hitungLuasPersegiPanjang() {
+    const panjang = document.getElementById('panjang').value;
+    const lebar = document.getElementById('lebar').value;
+    const luaspersegipanjang = panjang * lebar;
+    const outputText = `Luas = ${panjang} x ${lebar} = ${luaspersegipanjang}`;
+    document.getElementById('luaspersegipanjang').innerText = outputText;
+}
+
+function hitungKelilingPersegiPanjang() {
+    const panjang = document.getElementById('panjang').value;
+    const lebar = document.getElementById('lebar').value;
+        // Konversi nilai panjang dan lebar menjadi angka dlu
+    const panjangNumber = parseInt(panjang);
+    const lebarNumber = parseInt(lebar);
+    const kelilingpersegipanjang = 2 * (lebarNumber + panjangNumber) ;
+    const outputText = `Keliling = 2 x (${panjang} + ${lebar}) = ${kelilingpersegipanjang}`;
+    document.getElementById('kelilingpersegipanjang').innerText = outputText;
+}
+
+function resetInput2() {
+    document.getElementById('panjang').value = '';
+    document.getElementById('lebar').value = '';
+    document.getElementById('luaspersegipanjang').innerText = '';
+    document.getElementById('kelilingpersegipanjang').innerText = '';
 }
